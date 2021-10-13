df3f9d530b145585e55fb968a0b3f323448eab25
Anton <antkumachev@gmail.com>
2021-10-13 21:30:50 +0300

    Добавьте файлы проекта.

---
+++ Markov/MarkovCrypter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace Markov
+{
+    public class MarkovCrypter
+    {
+        private static readonly string[][] replaces = new string[4][]
+        {
+            new string[] { "a0", "0a" },
+            new string[] { "a1", "1b" },
+            new string[] { "b0", "1a" },
+            new string[] { "b1", "0b" }
+        };
+
+        public string Encrypt(byte value)
+        {
+            string bin = 'a' + Convert.ToString(value, 2);
+            int index = 0;
+            int length = replaces.Length;
+
+            while (bin.Last() != 'a' && bin.Last() != 'b')
+            {
+                bin = bin.Replace(replaces[index % length][0], replaces[index % length][1]);
+                index += 1;
+            }
+
+            value = Convert.ToByte(bin[0..^1], 2);
+            return $"{value}{bin.Last()}";
+        }
+
+        public byte Decrypt(string value)
+        {
+            if (value.Last() != 'a' && value.Last() != 'b')
+                throw new ArgumentException(message: "Encrypted value must end with a or b", paramName: nameof(value));
+
+            int index = 0;
+            int length = replaces.Length;
+            byte bin = Convert.ToByte(value[0..^1]);
+            value = Convert.ToString(bin, 2) + value.Last();
+
+            while (value.First() != 'a' && value.First() != 'b')
+            {
+                value = value.Replace(replaces[index % length][1], replaces[index % length][0]);
+                index += 1;
+            }
+
+            if (value.First() != 'a')
+                throw new CryptographicException();
+
+            return Convert.ToByte(value[1..], 2);
+        }
+    }
+}
---
+++ MarkovTest/MarkovCrypterTests.cs
@@ -0,0 +1,60 @@
+using Markov;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace MarkovTest
+{
+    public class MarkovCrypterTests
+    {
+        private MarkovCrypter crypter;
+
+        [SetUp]
+        public void Setup()
+        {
+            crypter = new();
+        }
+
+        [Test]
+        public void EncryptTest()
+        {
+            Assert.AreEqual(crypter.Encrypt(115), "74b");
+        }
+
+        [Test]
+        public void DecryptTest()
+        {
+            Assert.AreEqual(crypter.Decrypt("74b"), 115);
+        }
+
+        [Test]
+        public void DecryptNotProperValue()
+        {
+            Assert.Catch<CryptographicException>(() =>
+            {
+                crypter.Decrypt("74a");
+            });
+        }
+
+        [Test]
+        public void DecryptBigValue()
+        {
+            Assert.Catch<Exception>(() =>
+            {
+                crypter.Decrypt("256a");
+            });
+        }
+
+        [Test]
+        [Timeout(1000)]
+        public void NotInfiniteLoop()
+        {
+            Assert.Catch<ArgumentException>(() =>
+            {
+                crypter.Decrypt("74");
+            });
+        }
+    }
+}
